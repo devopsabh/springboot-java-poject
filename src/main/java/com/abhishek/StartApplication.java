3c40eabc61eb0b5d6ee465111892aa584d9c980b
devopsabh <devabhau1@proton.me>
2026-03-15 20:12:11 +0530

    Enhance StartApplication for Tomcat compatibility
    Updated the StartApplication class to extend SpringBootServletInitializer for Tomcat compatibility and modified the index method to reflect new application details.

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -2,23 +2,30 @@ package com.abhishek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
-public class StartApplication {
+public class StartApplication extends SpringBootServletInitializer { // 1. Added extends
+
+    // 2. Added this method for Tomcat compatibility
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StartApplication.class);
+    }
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "This is a SpringBoot Static Web Application");
-        model.addAttribute("msg", "Application Is Deployed To Kuberneets");
+        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
+        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
-
 }
